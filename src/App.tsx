eed6565b2653ee7a38f0d6ca22df3b6a0dd236e0
ILog <ilog2000@gmail.com>
2023-10-31 11:16:03 +0100

    Fix format on save for TypeScript

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import './App.css'
-import { QueryClient, QueryClientProvider } from "react-query"
+import { QueryClient, QueryClientProvider } from 'react-query'
 import { MantineProvider } from '@mantine/core'
 import { Converter } from './components/Converter/Converter'
 
-const queryClient = new QueryClient({});
+const queryClient = new QueryClient({})
 
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <MantineProvider  defaultColorScheme="light">
+      <MantineProvider defaultColorScheme="light">
         <Converter />
       </MantineProvider>
     </QueryClientProvider>
