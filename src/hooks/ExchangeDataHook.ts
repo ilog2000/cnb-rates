ed632fbd2b2e3636bc5837a5927c0e915b4ed667
ILog <ilog2000@gmail.com>
2023-11-03 11:32:54 +0100

    Configure proxy for /cnb request

--- src/hooks/ExchangeDataHook.ts
+++ src/hooks/ExchangeDataHook.ts
@@ -6,7 +6,6 @@ import { useQuery } from 'react-query'
 export function useExchangeData(): ExchangeData {
   const { isError, error, data } = useQuery('exchangeData', async () => {
     // TODO: make server side API endpoint to fetch data from CNB due to CORS policy on the CNB server.
-    // At the moment, the proxy in Vite server is used to overcome CORS restrictions (see vite.config.ts).
     const textDate = await fetchExchangeData('/cnb')
     const data = parseTextData(textDate)
     return data
